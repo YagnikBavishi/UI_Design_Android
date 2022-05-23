7d395b58cca3705c3b724a298f2f9f6e55cadd68
YagnikBavishi <yagnikbavishi003@gmail.com>
2022-05-23 12:01:49 +0530

    Authentication Screen Completed

--- app/src/main/java/com/example/myapplication/activity/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/activity/MainActivity.kt
@@ -1,10 +1,12 @@
-package com.example.myapplication
+package com.example.myapplication.activity
 
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
 
 
+import com.example.myapplication.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/example/myapplication/constants/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication.constants
+
+object Constants {
+    const val ZERO = 0
+    const val TWENTY_FIVE = 25
+    const val TWENTY_TWO = 22
+    const val THIRTY = 30
+    const val THIRTY_FOUR = 34
+}
